43d91f07aa792c4f1956c04c7182492f597a255e
fritzgt <tuanis@live.com>
2020-08-07 08:28:15 +0300

    completed stack

--- RPN/Stack.swift
+++ RPN/Stack.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
-struct Stack{
+struct Stack<Element>: ExpressibleByArrayLiteral {
+    
+    //MARK: - Properties
+    typealias ArrayLiteralElement = Element//USe to decribe the type inside the array literal
+    
+    private(set) var elements: [Element]//Makes the setter private to be set only inside the struct but readonly outside
+    
+    init(arrayLiteral elements: Element...) {//Initiate the array
+        self.elements = elements
+    }
+    
+    //MARK: - Methods
+    
+    //Adds element to the array
+    mutating func push(_ element: Element) {//Mutating because we are changing a property in a struct
+        elements.append(element)
+    }
+    
+    //Removes the last element to the array
+    mutating func pop() -> Element? {
+        return elements.popLast()
+    }
+    
+    //Shows the item at the top of the stack
+    func peek() -> Element? {
+        return elements.last
+    }
     
 }
