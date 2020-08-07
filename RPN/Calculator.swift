2705353bd0cf32251415268d26cdd14c526be314
fritzgt <tuanis@live.com>
2020-08-07 08:54:07 +0300

    Created the calculator struct

---
+++ RPN/Calculator.swift
@@ -0,0 +1,53 @@
+//
+//  Calculator.swift
+//  RPN
+//
+//  Created by FGT MAC on 8/7/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import Foundation
+
+struct Calculator {
+    
+    enum Operator {
+        case add
+        case subtract
+        case multiply
+        case divide
+    }
+    
+    private var stack: Stack<Double> = [0.0,0.0]//Init an array with double type
+    
+    init() {
+        
+    }
+    
+    //Adds a number to the stack
+    mutating func push(_ number: Double){
+        stack.push(number)
+    }
+    
+    mutating func push(operator: Operator){
+        
+        let operand2 = stack.pop() ?? 0.0//removes the last num on the stack and saves it to the variable
+        let operand1 = stack.pop() ?? 0.0//removes and save the element on the button of the stack
+        
+        var result: Double
+        
+        switch `operator` {//By using backtics we tell xcode to use the oprator parameter from the function
+        case .add:
+            result = operand1 + operand2
+        case .subtract:
+            result = operand1 - operand2
+        case .multiply:
+            result = operand1 * operand2
+        case .divide:
+            result = operand1 / operand2
+        }
+        
+        stack.push(result)//Since we pop the two items in the stack we can now push the result to the stack to chain and continue adding numbers if wanted like 2 + 5 = 7, 7 + 3 = 10 etc
+        
+    }
+    
+}
