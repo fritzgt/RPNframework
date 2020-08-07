6d514efae90b14ad3ac0b860fdd6bd802e2877be
fritzgt <tuanis@live.com>
2020-08-07 10:22:49 +0300

    import and implement digitAccumulator

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import RPN
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    private var calculator = Calculator()
+    
+    private var digitAccumulator = DigitAccumulator()
+    
+    
+    //MARK: - View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func numberButtonTap(_ sender: UIButton){
+        try? digitAccumulator.add(digit: .number(sender.tag))
+    }
 
 }
 
